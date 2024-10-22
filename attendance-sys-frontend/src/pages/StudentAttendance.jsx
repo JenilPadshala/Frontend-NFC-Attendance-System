f29e8782e84edc7594b5e1a7ece0110d2b8d5b72
Jenil Padshala <jenilpadshala@gmail.com>
2024-10-22 11:40:40 +0530

    Added Student Attendance Table
    The student can login and view his/her attendance in all the course they are enrolled in.

--- attendance-sys-frontend/src/pages/StudentAttendance.jsx
+++ attendance-sys-frontend/src/pages/StudentAttendance.jsx
@@ -1,31 +1,56 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import StudentNavbar from "../components/StudentNavbar";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import AttendanceTable from "../components/AttendanceTable";
+const backendurl = import.meta.env.VITE_BACKEND_URL;
 export default function StudentAttendance() {
   const location = useLocation();
   const { studentData } = location.state || {};
+  const [attendanceData, setAttendanceData] = useState([]);
+
+  useEffect(() => {
+    //Fetch attendance data for the student from the backend
+    const fetchAttendance = async () => {
+      axios
+        .get(`${backendurl}/student/${studentData.student_id}/attendance`)
+        .then((response) => {
+          if (response.status === 200) {
+            console.log(response.data);
+            setAttendanceData(response.data);
+          }
+        })
+        .catch((error) => console.error(error));
+    };
+    fetchAttendance();
+  }, []);
 
   return (
-    <div className="container mt-5">
-      <h1>Student Details</h1>
-      {studentData ? (
-        <div>
-          <p>
-            <strong>Name:</strong> {studentData.first_name}{" "}
-            {studentData.last_name}
-          </p>
-          <p>
-            <strong>Student Id:</strong> {studentData.student_id}
-          </p>
-          <p>
-            <strong>Email:</strong> {studentData.email}
-          </p>
-          <p>
-            <strong>NFC ID:</strong> {studentData.nfc_tag_id}
-          </p>
-        </div>
-      ) : (
-        <p>No data available.</p>
-      )}
+    <div>
+      <StudentNavbar studentData={studentData} />
+      <div className="container mt-5">
+        <h1>Attendance Details</h1>
+        <br />
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Course Title</th>
+              <th scope="col">Attendance Percentage</th>
+            </tr>
+          </thead>
+          <tbody>
+            {attendanceData.map((attendance, index) => (
+              <AttendanceTable
+                key={index}
+                course_name={attendance.course_name}
+                percentage={attendance.attendance_percentage}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
